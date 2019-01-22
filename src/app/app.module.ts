643f1ab0702909be64c08ef82586468d0aba77e0
jayanti18 <jayantirajesh@yahoo.com>
2019-01-22 16:27:58 -0500

    updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { ContactComponent } from './herokus/contact/contact.component';
+import { ContactDetailsComponent } from './herokus/contact-details/contact-details.component';
+import { ContactListComponent } from './herokus/contact-list/contact-list.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // ContactComponent,
+    ContactDetailsComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/herokus/contact/contact.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact';
+import { ContactService } from '../contact.service';
+import { ContactDetailsComponent } from '../contact-details/contact-details.component';
+
+@Component({
+  selector: 'contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css'],
+  providers: [ContactService]
+})
+
+export class ContactListComponent implements OnInit {
+
+  herokus: Contact[]
+  selectedContact: Contact
+
+  constructor(private contactService: ContactService) { }
+
+  ngOnInit() {
+     this.contactService
+      .getContacts()
+      .then((herokus: Contact[]) => {
+        this.herokus = herokus.map((contact) => {
+          if (!contact.phone) {
+            contact.phone = {
+              mobile: '',
+              work: ''
+            }
+          }
+          return contact;
+        });
+      });
+  }
+
+  private getIndexOfContact = (contactId: String) => {
+    return this.herokus.findIndex((contact) => {
+      return contact._id === contactId;
+    });
+  }
+
+  selectContact(contact: Contact) {
+    this.selectedContact = contact
+  }
+
+  createNewContact() {
+    var contact: Contact = {
+      name: '',
+      email: '',
+      phone: {
+        work: '',
+        mobile: ''
+      }
+    };
+
+    // By default, a newly-created contact will have the selected state.
+    this.selectContact(contact);
+  }
+
+  deleteContact = (contactId: String) => {
+    var idx = this.getIndexOfContact(contactId);
+    if (idx !== -1) {
+      this.herokus.splice(idx, 1);
+      this.selectContact(null);
+    }
+    return this.herokus;
+  }
+
+  addContact = (contact: Contact) => {
+    this.herokus.push(contact);
+    this.selectContact(contact);
+    return this.herokus;
+  }
+
+  updateContact = (contact: Contact) => {
+    var idx = this.getIndexOfContact(contact._id);
+    if (idx !== -1) {
+      this.herokus[idx] = contact;
+      this.selectContact(contact);
+    }
+    return this.herokus;
+  }
+}
